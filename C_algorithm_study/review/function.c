9e9afafdcaa259c1326a475daf77b5fd5fbcd0de
Junhyun17 <opas293@naver.com>
2026-09-06 21:17:20 +0900

    공부내용 추가

---
+++ C_algorithm_study/review/function.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int add(int a, int b);
+
+int main() {
+	int num1 = 3;
+	int num2 = 4;
+	int result;
+
+	result = add(num1, num2);
+	printf("%d", result);
+	return 0;
+}
+
+int add(int a, int b) {
+	return (a + b);
+}
