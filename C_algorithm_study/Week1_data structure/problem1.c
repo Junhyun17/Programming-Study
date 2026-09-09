06c636ac266497103244f3434b7e6aae029b9ad9
Junhyun17 <opas293@naver.com>
2026-09-10 01:37:20 +0900

    1주차 과제 코드

---
+++ C_algorithm_study/Week1_data structure/problem1.c
@@ -0,0 +1,143 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    char elem;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+void initList(Node* header, Node* trailer) {
+    header->prev = NULL;
+    header->next = trailer;
+
+    trailer->prev = header;
+    trailer->next = NULL;
+}
+
+void add(Node* list, int r, char e) {
+    if (r <= 0) {
+        printf("invalid position\n");
+        return;
+    }
+
+    for (int i = 1; i < r; i++) {
+        list = list->next;
+
+        if (list->next == NULL) {
+            printf("invalid position\n");
+            return;
+        }
+    }
+
+    Node* newnode = (Node*)malloc(sizeof(Node));
+
+    newnode->elem = e;
+    newnode->prev = list;
+    newnode->next = list->next;
+
+    list->next->prev = newnode;
+    list->next = newnode;
+}
+
+void delete(Node* list, int r) {
+    if (r <= 0) {
+        printf("invalid position\n");
+        return;
+    }
+
+    for (int i = 0; i < r; i++) {
+        list = list->next;
+
+        if (list->next == NULL) {
+            printf("invalid position\n");
+            return;
+        }
+    }
+
+    Node* delnode = list;
+
+    delnode->prev->next = delnode->next;
+    delnode->next->prev = delnode->prev;
+
+    free(delnode);
+}
+
+void get(Node* list, int r) {
+    if (r <= 0) {
+        printf("invalid position\n");
+        return;
+    }
+
+    for (int i = 0; i < r; i++) {
+        list = list->next;
+
+        if (list->next == NULL) {
+            printf("invalid position\n");
+            return;
+        }
+    }
+
+    printf("%c\n", list->elem);
+}
+
+void print(Node* list) {
+    list = list->next;
+
+    while (list->next != NULL) {
+        printf("%c", list->elem);
+        list = list->next;
+    }
+
+    printf("\n");
+}
+
+void freeList(Node* header) {
+    Node* current = header;
+
+    while (current != NULL) {
+        Node* next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+int main(void) {
+    Node* header = (Node*)malloc(sizeof(Node));
+    Node* trailer = (Node*)malloc(sizeof(Node));
+
+    int count, pos;
+    char element, command;
+
+    initList(header, trailer);
+
+    scanf("%d", &count);
+
+    for (int i = 0; i < count; i++) {
+        scanf(" %c", &command);
+
+        if (command == 'A') {
+            scanf("%d %c", &pos, &element);
+            add(header, pos, element);
+        }
+
+        else if (command == 'D') {
+            scanf("%d", &pos);
+            delete(header, pos);
+        }
+
+        else if (command == 'G') {
+            scanf("%d", &pos);
+            get(header, pos);
+        }
+
+        else if (command == 'P') {
+            print(header);
+        }
+    }
+
+    freeList(header);
+
+    return 0;
+}
---
+++ C_algorithm_study/Week1_data structure/problem2.c
@@ -0,0 +1,76 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode {
+    int elem;
+    struct TreeNode* left;
+    struct TreeNode* right;
+} TreeNode;
+
+TreeNode* makeTree() {
+    int x, y, z;
+
+    scanf("%d %d %d", &x, &y, &z);
+
+    TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
+
+    node->elem = x;
+    node->left = NULL;
+    node->right = NULL;
+
+    if (y != 0)
+        node->left = makeTree();
+
+    if (z != 0)
+        node->right = makeTree();
+
+    return node;
+}
+
+void search(TreeNode* root, char* path) {
+    TreeNode* p = root;
+
+    printf(" %d", p->elem);
+
+    for (int i = 0; path[i] != '\0'; i++) {
+        if (path[i] == 'L')
+            p = p->left;
+        else if (path[i] == 'R')
+            p = p->right;
+
+        printf(" %d", p->elem);
+    }
+
+    printf("\n");
+}
+
+void freeTree(TreeNode* root) {
+    if (root == NULL)
+        return;
+
+    freeTree(root->left);
+    freeTree(root->right);
+
+    free(root);
+}
+
+int main() {
+    int n, s;
+    char path[101];
+
+    scanf("%d", &n);
+
+    TreeNode* root = makeTree();
+
+    scanf("%d", &s);
+
+    for (int i = 0; i < s; i++) {
+        scanf("%s", path);
+        search(root, path);
+    }
+
+    freeTree(root);
+
+    return 0;
+}
